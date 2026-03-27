29e5b538175ebd6f186dbf3a68341180ba4bdc9d
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-03-27 10:12:03 +0500

    setup tabs in the app

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,11 +1,77 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants";
 
 const TabLayout = () => {
   return (
-    <View>
-      <Text>TabLayout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: "#cdcde0",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#f0f0f0",
+          height: 56,
+          paddingTop: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="cart"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Feather
+              name={focused ? "shopping-cart" : "shopping-cart"}
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="favourites"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
